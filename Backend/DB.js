73e4b328dc9b7ba75c727b58a30747440caa34f2
Shubham2001sharma <sharmashubu4600@gmail.com>
2024-06-26 11:07:44 +0330

    last-final-commit

--- Backend/DB.js
+++ Backend/DB.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
 // Connect to MongoDB database
-mongoose.connect("mongodb://localhost:27017/FINAL")
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+mongoose.connect('mongodb://localhost:27017/yourDatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('Error connecting to MongoDB:', err.message));
 
 // Define schema
 const schema = new mongoose.Schema({
